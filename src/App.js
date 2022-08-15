ada9c7353a9ce69fd8a8a64e5ad2b607e2c837a9
Dusan <dusanvulic11@gmail.com>
2022-08-15 20:22:15 +0200

    nastavak ucenja

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useState } from "react";
+
 function App() {
   return (
-    <div className="container">
-      <h3>redux toolkit</h3>
+    <div className="App">
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              <div className="reservation-card-container">Laith Harb</div>
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input />
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          <div className="customer-food-card-container">
+            <p>Selena Gomez</p>
+            <div className="customer-foods-container">
+              <div className="customer-food"></div>
+              <div className="customer-food-input-container">
+                <input />
+                <button>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
