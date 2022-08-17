91b9f776c53bf2e6999bc660022d201b610be01d
Dusan <dusanvulic11@gmail.com>
2022-08-17 12:49:42 +0200

    restored for better learning

--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import ReservationReducer from "../features/reservationSLice";
-
-export const store = configureStore({
-    reducer: { reservations: ReservationReducer },
-});
